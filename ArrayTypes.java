e42a43f8476e3feb54bfa0863b32359946b22cac
Anthony Le <116751998+Ayprusss@users.noreply.github.com>
2023-01-15 20:48:05 -0500

    Add files via upload

---
+++ ArrayTypes.java
@@ -0,0 +1,20 @@
+import java.util.Arrays; 
+
+public class ArrayTypes {
+    public static void main(String[] args) {
+        int[] numbers = new int[5]; //due to a list being a reference type, we need to use the new opeartor. For the array, we would also have to input what kind of data it is (int, double, etc), and then after we input how many new numbers we want in the array. 
+        numbers[0] = 1;
+        numbers[1] = 2;// older syntax to intialize an array though.
+        System.out.println(numbers); //we can't print a list like this. By doing so, it's only printing the list by its address for the memory space of the list not the actual values themselves.
+        Arrays.toString(numbers);
+        System.out.println(Arrays.toString(numbers));
+
+        int[] nums = {2,3,5,1,4};
+        //nums.length; // will print out the length of the list itself; how many values are in the list
+        System.out.println(nums.length); //once an array is made, its length cannot be changed or altered later on. So unlike python, no appending or insertion can be cone; it's only allowed if the list is made to hold a certain amount of numbers/
+
+
+        Arrays.sort(nums);
+        System.out.println((Arrays.toString(nums)));
+    }
+}
